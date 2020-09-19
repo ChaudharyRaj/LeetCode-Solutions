e6d64bd76487509ec009b14f53a1b86339e21aab
RAj Kumar Chaudhary <chaudharyrajkumar173@gmail.com>
2020-09-19 19:52:04 +0530

    Update with Tabulation method

--- Dynamic Programing/Maximal Square.java
+++ Dynamic Programing/Maximal Square.java
@@ -11,6 +11,9 @@ Input:
 
 Output: 4*/
 
+
+// Mamoization Method
+
 class Solution {
     public int maximalSquare(char[][] matrix) {
         
@@ -38,5 +41,31 @@ class Solution {
         return dp[i][j]  =  1 + Math.min(findsquare(matrix, i+1, j+1,dp),
                                 Math.min(findsquare(matrix, i+1,j,dp),
                                          findsquare(matrix, i, j+1, dp)));
+    }
+}
+
+
+// Tabulation Method:
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix.length == 0) return 0;
+        
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        int max = 0;
+        for(int i =0; i< matrix.length; i++){
+            for(int j = 0; j <matrix[i].length; j++){
+                
+                if(i == 0 || j == 0)dp[i][j] = matrix[i][j] == '0' ? 0: 1 ;
+                
+                else{
+                    if(matrix[i][j] == '0') continue;
+                    
+                     dp[i][j] = 1 + Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+                max = Math.max(max,dp[i][j]);
+            }
+        }
+        return max*max;
     }
 }
